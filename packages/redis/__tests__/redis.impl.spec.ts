e5ec18bc9b55ab86e8d966f4af9a7a68edb9ff96
ayo <a2yomidiscord@gmail.com>
2025-07-18 15:23:47 +0300

    chore: new pkg

--- packages/redis/__tests__/redis.impl.spec.ts
+++ packages/redis/__tests__/redis.impl.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect } from 'vitest';
-import { createRedisRest } from '../src';
+import { createRedisRest } from '../../ts-fetcher/src';
 
 const fakeConnect = {
   host: 'redis',
