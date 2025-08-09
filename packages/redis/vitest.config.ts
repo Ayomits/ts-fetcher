93bd209ba0ebd6cfb68d8a8c80c49370c13d0101
ayo <a2yomidiscord@gmail.com>
2025-08-10 01:33:02 +0300

    chore: remove package redis

--- packages/redis/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineProject, mergeConfig } from 'vitest/config';
-import configShared from '../../vitest.config.js';
-
-export default mergeConfig(configShared, {
-  defineProject: defineProject({
-    test: {},
-  }),
-});
